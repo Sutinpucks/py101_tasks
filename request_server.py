a05d072d34507c5e1595fd0010e2db58b058c9ef
Sutinpucks <woesse@gmail.com>
2020-11-14 18:45:47 +0300

    done

---
+++ request_server.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import logging
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format="%(levelname)s %(asctime)s %(message)s")
+
+URL = os.getenv('SITE')
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+FROM_NUMBER = os.getenv('FROM_NUMBER')
+TO_NUMBER = os.getenv('TO_NUMBER')
+MESSAGE_TEXT = f'Server is down {URL}'
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+def sms_send():
+        message = client.messages.create(
+            body=MESSAGE_TEXT + str(response.ok),
+            from_=FROM_NUMBER,
+            to=TO_NUMBER,
+            )
+        logging.info(f'Message succesfully sent. SID: {message.sid}')
+       
+response = requests.get(URL)
+while response.ok:
+    logging.info('Checking server')
+    time.sleep(60)
+else:
+    logging.warning('Server is down, sending sms')
+    sms_send()
+    
+        
+    
